a8b92fa6ca6621d10e1b391813732f277011eaea
Kist221 <Kist221@gmail.com>
2017-11-09 17:56:16 -0700

    start over

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,28 +5,50 @@
     var guess = document.getElementById("guess");
     var win = document.getElementById("win");
     var lose = document.getElementById("lose");
+    var active = document.getElementById("activeWord");
     var cWin = 0;
     var cLose = 0;
     var cGuess = 9;
+    var uS = [];
+    var activeChar = [];
     var tried = [];
+    var userGuess;
 
     // Randomly chooses a choice from the options array. This is the Computer's guess.
     var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)];
 
     // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) 
-    {
+    document.onkeyup = function(event) {
 
     // Determines which key was pressed.
-    var userGuess = event.key;
+    userGuess = event.key;
+
+    // Clear startup language
+    active.innerHTML = "";
+
+    // Runs on new game / No computer guess
+    if (activeChar.length === 0) {
+
+      for (var i = 0; i < computerGuess.length; i++) {
+
+        // convert word to characters array
+        activeChar.push(computerGuess.charAt([i]));
+
+        // Runs for length of array
+        if (activeChar[i] !== -1) {
+
+        // Display active characters as underscores
+        uS.push("_");
+        
+        }
+
+      }
 
     }
 
+    active.append(uS.join(" "));
 
-// Display computer random gameChoice as blanks
-
-
-// Collect userInput into array and check against words (use .charAt[?])
+  
 
 
-// Fill in inputs into blanks
+    }
