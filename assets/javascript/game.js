30f8f4fea6648e0c35f1bc82dbbb15c225116343
Kist221 <Kist221@gmail.com>
2017-11-09 23:28:53 -0700

    further logic, basic complete, add scoring then styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,6 +17,8 @@
     // Randomly chooses a choice from the options array. This is the Computer's guess.
     var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)];
 
+    
+
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
 
@@ -26,29 +28,91 @@
     // Clear startup language
     active.innerHTML = "";
 
-    // Runs on new game / No computer guess
-    if (activeChar.length === 0) {
+        // Runs on new game / No computer guess
+        if (activeChar.length === 0)
+        {
 
-      for (var i = 0; i < computerGuess.length; i++) {
+          // Loop length of word
+          for (var i = 0; i < computerGuess.length; i++)
+          {
 
-        // convert word to characters array
-        activeChar.push(computerGuess.charAt([i]));
+            // convert word to characters array
+            activeChar.push(computerGuess.charAt([i]));
 
-        // Runs for length of array
-        if (activeChar[i] !== -1) {
+            // Runs for length of array
+            if (activeChar[i] !== -1)
+            {
 
-        // Display active characters as underscores
-        uS.push("_");
+            // Store active characters as underscores
+            uS.push("_");
+            
+            }
+
+          }
+
+        }
+
+
+
+    // Displays individual characters as _ replacing commas with spaces
+    active.append(uS.join(" "));   
+    
+      // Only run if not duplicate input
+      if (tried.indexOf(userGuess) === -1)
+      {
+
+        // Only store wrong guesses for input list
+        if (activeChar.indexOf(userGuess) === -1) {
+
+        // Log user inputs
+        tried.push(userGuess);
+
+        // Print Used Letters
+        user.innerHTML = tried.join(", ");
+
+        }
+
+            // compare correct values and fill in blanks
+            for (var i = 0; i < activeChar.length; i++) {
+
+                  // compare user guess input to current word char index
+                  if (userGuess === activeChar[i])
+                  {
+
+                    console.log("userGuess = activeChar[i]");
+
+                    // update array with matching input
+                    uS[i] = activeChar[i];
+
+                    // write to document
+                    active.innerHTML = uS.join(" ");
+
+                  }
+                
+            }
         
+            // if incorrect input 
+            if (activeChar.indexOf(userGuess) === -1 && cGuess > 0) 
+            {
+
+              console.log("subtract guess count")
+
+              cGuess--;
+
+              guess.innerHTML = cGuess;
+
+            }
+
         }
 
       }
 
-    }
-
-    active.append(uS.join(" "));
-
-  
 
 
-    }
+
+
+
+
+
+
+
